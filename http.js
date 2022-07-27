1c73dbda1593b15a543e50b6ee75f9a57c4fb960
root <1255815114@qq.com>
2022-07-27 18:35:17 +0800

    test

--- http.js
+++ http.js
@@ -1,28 +1,42 @@
 const http = require('http')
 const fs = require('fs')
 const path = require('path')
+var url = require('url');
 
 const server = http.createServer()
 
 server.on('request', function(req,res) {
-    console.log('someone vist server')
-    const url = req.url
-    if (url === '/' && req.method == 'GET')
-    {
-        res.setHeader('Content-Type', 'text/html;charset=utf-8')
-        res.end('success 访问成功')
-        console.log('success')
-    } else if (url === '/map') {
-        res.setHeader('Content-Type', 'text/html;charset=utf-8')
-        fs.readFile(__dirname+'/Map.html','utf-8',function(err,data){
-            if (err) {
-                return console.log('读取失败'+err)
-            }
-            res.write(data);
-            res.end();
-            console.log('访问了map')
-        })
-    }
+     // 解析请求，包括文件名
+   const pathname = url.parse(req.url).pathname;
+   const postfix = pathname.match(/(\.[^.]+|)$/)[0];//取得后缀名
+   // 输出请求的文件名
+   console.log("Request for " + pathname + " received.");
+   // 从文件系统中读取请求的文件内容
+   fs.readFile(__dirname+pathname, function (err, data) {
+      if (err) {
+         console.log(err);
+        // HTTP 状态码: 404 : NOT FOUND
+         // Content Type: text/plain
+         res.writeHead(404, {'Content-Type': 'text/html; charset=utf-8'});
+      }else{            
+         // HTTP 状态码: 200 : OK
+         // Content Type: text/plain
+         console.log(postfix);
+         if(postfix==='html'){
+            res.writeHead(200, {'Content-Type': 'text/html'});   
+         }else if(postfix==='css'){
+            res.writeHead(200, {'Content-Type': 'text/css'});
+         }
+         else if(postfix==='js'){
+            res.writeHead(200, {'Content-Type': 'application/javascript'});
+         }else{
+         }
+         // 响应文件内容
+         res.write(data.toString());       
+      }
+      //  发送响应数据
+      res.end();
+   });
 })
 
 server.listen(8111, function() {
